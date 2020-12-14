6eff5f0e77bde7bbec79306b74b8c951b3f98c28
insta <insta@DESKTOP-IAVPL5C>
2020-12-14 18:22:07 +0900

    Practice09

---
+++ src/com/javaex/ex02/FriendApp.java
@@ -0,0 +1,23 @@
+package com.javaex.ex02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FriendApp {
+
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("친구 3명을 등록해 주세요.");
+    	
+    	List<Friend> fList = new ArrayList<Friend>();
+    	
+    	for(int i =0 ; i<3; i++) {
+    		
+    	}
+    	
+    	
+    	sc.close();
+    }
+
+}
